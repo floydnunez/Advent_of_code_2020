f23c0f5bcc7a950a05914748db58ada77c3919fe
jp.nunnez <jp.nunnez@continuum.cl>
2020-12-02 11:40:28 -0300

    Adding scanf to the solutions

--- day_02/02full_scanf.js
+++ day_02/02full_scanf.js
@@ -1,12 +1,21 @@
+// https://www.reddit.com/r/adventofcode/comments/k52psu/2020_day_02_solutions/gedehb1/?utm_source=reddit&utm_medium=web2x&context=3
 const fs = require('fs');
 const sscanf = require('scanf').sscanf;
 
 const pass_rules = fs.readFileSync('02a.txt', 'utf8').split('\n');
-const length = pass_rules.length;
 
 const rules = [];
 
 pass_rules.forEach(prk => rules.push(sscanf(prk, '%d-%d %s: %s', 'min', 'max', 'char', 'pass')));
+
+var valids_2a = 0;
+rules.forEach(rule => {
+    const occurrences = rule.pass.split(rule.char).length - 1;
+    if (occurrences >= rule.min && occurrences <= rule.max) {
+        valids_2a++;
+    }
+});
+console.log('valid passwords: ', valids_2a, 'out of', length);
 
 function validate(rule) {
     if (!(rule.pass[rule.min - 1] === rule.char && rule.pass[rule.max - 1] === rule.char)) {
@@ -20,4 +29,3 @@ function validate(rule) {
 const valids_2b = rules.filter(rule=> validate(rule));
 console.log('valid passwords: ', valids_2b.length, 'out of', length);
 console.log('fin 02a');
-//solution: 335
