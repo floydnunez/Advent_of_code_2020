dfc8932a7e5592d8bac8457d4bea6302259e73be
floydnunez <preempalver@gmail.com>
2020-12-04 10:14:02 -0300

    Day 2.

--- day_04/04a.js
+++ day_04/04a.js
@@ -2,33 +2,50 @@ console.log('hello advent! 04 a');
 
 const fs = require('fs');
 
-const all_numbers = fs.readFileSync('04a.txt', 'utf8');
-const arr = all_numbers.split('\n');
-const length = arr.length;
-
-const passwords = [];
-for (let index = 0; index < length; index++) {
-    const line = arr[index];
-    const parts = line.split(':');
-    const rules = parts[0].split(' ');
-    const limits = rules[0].split('-');
-    passwords[index] = {
-        min: parseInt(limits[0]),
-        max: parseInt(limits[1]),
-        char: rules[1],
-        pass: parts[1].trim()
-    };
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
 }
-var valids = 0;
+
+const all_data = fs.readFileSync('04a.txt', 'utf8');
+const all_passports = all_data.split('\n');
+const length = all_passports.length;
+
+passports = [];
+let passtotal = 0;
+let current_pass = '';
 for (let index = 0; index < length; index++) {
-    const rule = passwords[index];
-    const occurrences = rule.pass.split(rule.char).length - 1;
-    console.log('occurrences: ', occurrences, 'min:', rule.min, 'max:', rule.max);
-    if (occurrences >= rule.min && occurrences <= rule.max) {
-        console.log('solution: ', rule);
+    const line = all_passports[index];
+    if (isBlank(line)) {
+        passports[passtotal] = current_pass;
+        passtotal++;
+        current_pass = '';
+    } else {
+        current_pass += line;
+    }
+    console.log(line);
+}
+//gotta handle the last lines
+passports[passtotal] = current_pass;
+passtotal++;
+
+console.log('passports:', passports);
+console.log('all_passports.length:', all_passports.length);
+console.log('passports.length', passports.length);
+console.log(passports[passtotal-1]);
+let valids = 0;
+
+function valid_pass(p) {
+    if (p.includes('byr:') && p.includes('iyr:') && p.includes('eyr:') &&
+        p.includes('hgt:') && p.includes('hcl:') && p.includes('ecl:') &&
+        p.includes('pid:')) {  //&& p.includes('cid:')
+        return true;
+    }
+    return false;
+}
+
+for (let index = 0; index < passtotal; index++) {
+    if (valid_pass(passports[index])) {
         valids++;
     }
 }
-console.log('valid passwords: ', valids, 'out of', length);
-console.log('fin 02a');
-//solution: 591
+console.log('total valid passports: ',valids)
